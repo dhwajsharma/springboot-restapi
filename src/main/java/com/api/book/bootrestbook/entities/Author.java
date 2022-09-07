8f5fd58142b1af461552d64cc32adbc41fd2309c
Dhwaj Sharma <dhwajsharma10@gmail.com>
2022-09-07 15:11:29 +0530

    @JsonManagedReference, @JsonBackReference

--- src/main/java/com/api/book/bootrestbook/entities/Author.java
+++ src/main/java/com/api/book/bootrestbook/entities/Author.java
@@ -5,7 +5,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
 
@@ -18,6 +21,18 @@ public class Author {
     private String firstName;
     private String lastName;
     private String language;
+
+    @OneToOne(mappedBy = "author")
+    @JsonBackReference
+    private Book book;
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
 
     public int geAuthorId() {
         return authorId;
--- src/main/java/com/api/book/bootrestbook/entities/Book.java
+++ src/main/java/com/api/book/bootrestbook/entities/Book.java
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 @Table(name = "Books")
 public class Book {
@@ -20,6 +22,7 @@ public class Book {
     private String title;
 
     @OneToOne(cascade = CascadeType.ALL)
+    @JsonManagedReference
     private Author author;
 
     public Book(int id, String title, Author author) {
